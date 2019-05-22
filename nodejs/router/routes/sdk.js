155fa727e3769d1ceb74f1872a8ba0f5db140e6e
Andrew Lunde <andrew.lunde@sap.com>
2019-05-22 17:41:48 -0400

    add sdk imports

--- nodejs/router/routes/sdk.js
+++ nodejs/router/routes/sdk.js
@@ -1,6 +1,8 @@
 /*eslint no-console: 0, no-unused-vars: 0, no-shadow: 0, new-cap: 0*/
 "use strict";
 var express = require("express");
+var sdk-core = require("@sap/cloud-sdk-core");
+import { BusinessPartner } from "@sap/cloud-sdk-vdm-business-partner-service";
 
 module.exports = () => {
 	var app = express.Router();
